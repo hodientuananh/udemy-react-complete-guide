d126f5d908709052f01bb9a7e494dcfc59439a04
Tuan Anh Ho Dien <benkinmat@gmail.com>
2020-03-09 10:38:26 +0700

    add Person component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from "./Person/Person";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <h1>Hi, I am learning React</h1>
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    //   <h1>Hello World</h1>
+    // </div>
+    // React.createElement('div', {className: 'App'}, 'Hello World')
+      <div className='App'>
+        <h1>Hi, this is React App</h1>
+        <Person/>
+      </div>
   );
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = () => {
+    return <p>I am a Person with age: {Math.random()}</p>
+};
+
+export default person;
